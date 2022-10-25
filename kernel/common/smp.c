3e795e3014d78ba05588bf15dc0496b610d8bf4e
Artemis <xuechou@users.noreply.github.com>
2022-10-25 16:34:40 +0800

    lock

--- kernel/common/smp.c
+++ kernel/common/smp.c
@@ -34,6 +34,7 @@ void enable_smp_cores(void *addr)
 		 * _start of `start.S`. Then, what's the flag?
 		 * You only need to write one line of code.
 		 */
+		 secondary_boot_flag[i] = 1; // TODO: right ??
 
 		/* Lab4
 		 * The BSP waits for the currently initializing AP finishing
